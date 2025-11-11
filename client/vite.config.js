18e4a33bf403bb3e48907a6e3893dabfa94ca1ea
avgfx15 <avgfx15@gmail.com>
2025-11-11 08:34:37 +0530

    remove browser error with vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,4 +6,22 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js'],
+      'text/javascript': ['js'],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router'],
+          query: ['@tanstack/react-query'],
+          ui: ['react-quill-new', 'react-icons', 'react-toastify'],
+        },
+      },
+    },
+  },
 });
